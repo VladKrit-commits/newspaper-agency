d5234f31c75c770e3a1212aa26aeb7f07dfeebb7
Vlad Krit <kritvladislav98@gmail.com>
2024-01-29 12:02:12 +0200

    Set up a mdoels for projectÐ

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    # def get_absolute_url(self):
+    #     return reverse("agency:redactor-detail", kwargs={"pk": self.pk})
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()
+    published_date = models.DateField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, null=True, blank=True)
+    publishers = models.ManyToManyField(Redactor, related_name="newspaper")
+
+    def __str__(self):
+        return self.title
