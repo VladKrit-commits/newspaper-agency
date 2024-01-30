d5bc161aa5c40b08125286a904aa7aaec2ce9e55
Vlad Krit <kritvladislav98@gmail.com>
2024-01-30 23:32:48 +0200

    Configure settings, urls and fix some problems

--- NewspaperAgency/settings.py
+++ NewspaperAgency/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "debug_toolbar",
+    "crispy_forms",
     "agency",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'NewspaperAgency.urls'
@@ -131,3 +134,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
