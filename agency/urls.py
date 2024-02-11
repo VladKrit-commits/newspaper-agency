003b60940e390cfff54b055f29c5fd1d28fd3504
Vlad Krit <kritvladislav98@gmail.com>
2024-02-12 00:22:38 +0200

    Configure all url`s for project

--- agency/urls.py
+++ agency/urls.py
@@ -15,12 +15,15 @@ from .views import (
     TopicsUpdateView,
     TopicsDeleteView,
     RedactorsDetailView,
+    TopicsDetailView,
+    NewspapersDetailView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("newspapers/", NewspapersListView.as_view(), name="newspapers-list"),
     path("newspapers/create/", NewspapersCreateView.as_view(), name="newspapers-create"),
+    path("newspapers/<int:pk>/", NewspapersDetailView.as_view(), name="newspapers-detail"),
     path("newspapers/<int:pk>/update/", NewspapersUpdateView.as_view(), name="newspapers-update"),
     path("newspapers/<int:pk>/delete/", NewspapersDeleteView.as_view(), name="newspapers-delete"),
     path("redactors/", RedactorsListView.as_view(), name="redactors-list"),
@@ -30,6 +33,7 @@ urlpatterns = [
     path("redactors/<int:pk>/delete/", RedactorsDeleteView.as_view(), name="redactors-delete"),
     path("topics/", TopicsListView.as_view(), name="topics-list"),
     path("topics/create/", TopicsCreateView.as_view(), name="topics-create"),
+    path("topics/<int:pk>/", TopicsDetailView.as_view(), name="topics-detail"),
     path("topics/<int:pk>/update/", TopicsUpdateView.as_view(), name="topics-update"),
     path("topics/<int:pk>/delete/", TopicsDeleteView.as_view(), name="topics-delete"),
 
