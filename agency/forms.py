ccb24a323bae89377ed2054d9ea07b96e51e5c2a
Vlad Krit <kritvladislav98@gmail.com>
2024-02-12 00:15:48 +0200

    Made all required forms for project such as creation and search form`s

--- agency/forms.py
+++ agency/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-from django.core.exceptions import ValidationError
-
 from agency.models import Newspaper, Redactor
 
 
@@ -18,7 +16,7 @@ class NewspapersForm(forms.ModelForm):
 
 
 class NewspapersSearchForm(forms.Form):
-    model = forms.CharField(
+    title = forms.CharField(
         max_length=255,
         required=False,
         label="",
