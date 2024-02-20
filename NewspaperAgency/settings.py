cd65a807044c404a9acf25cb58350e0bc1345272
Vlad Krit <kritvladislav98@gmail.com>
2024-02-21 01:21:37 +0200

    fix settings for deployment

--- NewspaperAgency/settings.py
+++ NewspaperAgency/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+#DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "newspaper-agency-16y7.onrender.com"]
 
