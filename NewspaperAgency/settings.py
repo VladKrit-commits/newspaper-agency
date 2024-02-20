8bca4b23245f6a5b3e6287cd3a93401d43f57e5a
Vlad Krit <kritvladislav98@gmail.com>
2024-02-21 01:54:13 +0200

    fix settings for deployment (add host)

--- NewspaperAgency/settings.py
+++ NewspaperAgency/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["127.0.0.1", "newspaper-agency-16y7.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -135,9 +135,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles/'
 
-STATICFILES_DIRS = [BASE_DIR / "static",]
-
-ASSETS_ROOT = '/static/assets'
+STATICFILES_DIRS = (BASE_DIR, "agency/static",)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
