a3172c3943f93e8e2f83f4cb9a1767c5657914fe
Vlad Krit <kritvladislav98@gmail.com>
2024-02-21 02:52:38 +0200

    fix settings for deployment (add host)

--- NewspaperAgency/settings.py
+++ NewspaperAgency/settings.py
@@ -131,17 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = "staticfiles/"
 
-# STATICFILES_DIRS = [
-    # BASE_DIR / "F:/PortfolioProjects/NewspaperAgency/agency/static",
-# ]
-
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    '/opt/render/project/src/agency/static',
+    BASE_DIR / "static",
 ]
 
 
