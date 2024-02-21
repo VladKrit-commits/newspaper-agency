ad297a3c9d0e4fb0778c713ddca44f3c60ba0404
Vlad Krit <kritvladislav98@gmail.com>
2024-02-21 03:13:22 +0200

    fix settings for deployment (add host)

--- NewspaperAgency/settings.py
+++ NewspaperAgency/settings.py
@@ -131,13 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = 'static/'
 STATIC_ROOT = "staticfiles/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / 'static',
 ]
-
 
 ASSETS_ROOT = "/static/assets"
 # Default primary key field type
